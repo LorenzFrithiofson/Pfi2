e40440197a6859b3b8627f5c35ba6c6d95c39483
Lorenz <Lorenz@LoorisDator>
2015-02-18 10:45:45 +0100

    Är jag på rätt spår?

---
+++ A5_Lorenz_Frithiofson/src/ClockLogic.java
@@ -0,0 +1,22 @@
+
+public class ClockLogic {
+	
+	private DigitalClockGUI ClockGUI;
+	private int alarmHour;
+	private int alarmMinute;
+	
+		public ClockLogic(DigitalClockGUI clockIn) {
+		}
+		
+		public int setAlarm(int hours, int minutes){
+			this.hours = hours;
+			this.minute = minutes;
+		}
+		
+		public int clearAlarm(){
+			return this.Alarm;
+		}
+
+}
+
+
